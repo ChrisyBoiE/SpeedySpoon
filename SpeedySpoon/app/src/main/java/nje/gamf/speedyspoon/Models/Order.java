556ef92c2c2fc89302af382a94699e803d1cbd4c
pbeni01 <attilakriminal@gmail.com>
2025-05-12 16:18:23 +0200

    Order model update

--- SpeedySpoon/app/src/main/java/nje/gamf/speedyspoon/Models/Order.java
+++ SpeedySpoon/app/src/main/java/nje/gamf/speedyspoon/Models/Order.java
@@ -9,15 +9,19 @@ public class Order {
     private Integer totalAmount;
     private OrderDetail orderDetails;
     private String userID;
+    private String restaurant_id; 
+    private String order_id;      
 
     public Order(){}
 
-    public Order(String orderDate, String status, Integer totalAmount, OrderDetail orderDetails, String userID) {
+    public Order(String orderDate, String status, Integer totalAmount, OrderDetail orderDetails, String userID, String restaurant_id, String order_id) { 
         this.orderDate = orderDate;
         this.status = status;
         this.totalAmount = totalAmount;
         this.orderDetails = orderDetails;
         this.userID = userID;
+        this.restaurant_id = restaurant_id; 
+        this.order_id = order_id;          
     }
 
     public String getId() {
@@ -66,5 +70,21 @@ public class Order {
 
     public void setUserID(String userID) {
         this.userID = userID;
+    }
+
+    public String getRestaurant_id() {
+        return restaurant_id;
+    }
+
+    public void setRestaurant_id(String restaurant_id) {
+        this.restaurant_id = restaurant_id;
+    }
+
+    public String getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(String order_id) {
+        this.order_id = order_id;
     }
 }
